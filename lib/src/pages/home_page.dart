30e58111b3ec27f98dae57afa7eaf833f256f830
Victor Contreras <vjkc09@gmail.com>
2020-11-29 16:57:21 -0600

    feat: funcionalidad alert cerrar sesion

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   //const HomePage({Key key}) : super//(key: key);
 
+  final Color colorRosa = Color.fromRGBO(237, 0, 140, 1);
+
   @override
   Widget build(BuildContext context) {
 
@@ -14,11 +16,13 @@ class HomePage extends StatelessWidget {
           children:[
             Image.asset('assets/img/Imagotipo.png',),
             Text('Captura de votos', style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold,  color: Colors.black)),
-            Icon(
-              Icons.exit_to_app,
-              color: Color.fromRGBO(237, 0, 140, 1),
-              size: 24.0
-              
+            IconButton(
+              icon: Icon(
+                Icons.exit_to_app,
+                color: colorRosa,
+                size: 24.0               
+              ),
+              onPressed: () =>  _mostrarAlerta(context),
             )
           ],
           )
@@ -32,4 +36,39 @@ class HomePage extends StatelessWidget {
         ),
     );
   }
+
+   void _mostrarAlerta(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(20.0)),
+          title: Center(child: Text('Cerrar Sesión', style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold))),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Center(child: Text('¿Seguro que deseas cerrar sesión?', style: TextStyle(fontSize: 15.0,))),
+              Divider()
+            ],
+          ),
+          actions: [              
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                FlatButton(onPressed: () => Navigator.of(context).pop(), 
+                child: Text('Seguir aqui', style: TextStyle(color: colorRosa))),
+                FlatButton(onPressed: null, child: Text('|', style: TextStyle(color: colorRosa))),
+                FlatButton(onPressed: () {
+                  Navigator.pushReplacementNamed(context, 'login');
+                }, child: Text('Salir', style: TextStyle(color: colorRosa))),
+              ],
+            ),  
+          ],
+        );
+      }
+    );
+  }
+
+
 }
