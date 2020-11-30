d3d1ddcedbd6e0c8300e9785a4d19266babf6ba3
Victor Contreras <vjkc09@gmail.com>
2020-11-29 23:52:25 -0600

    feat: modelo para gestionar scanner

---
+++ lib/src/models/scan_model.dart
@@ -0,0 +1,31 @@
+// To parse this JSON data, do
+//
+//     final scanModel = scanModelFromJson(jsonString);
+
+import 'dart:convert';
+import 'package:meta/meta.dart'; 
+
+
+ScanModel scanModelFromJson(String str) => ScanModel.fromJson(json.decode(str));
+
+String scanModelToJson(ScanModel data) => json.encode(data.toJson());
+
+class ScanModel {
+    ScanModel({
+        this.id,
+        @required this.valor,
+    });
+
+    int id;
+    String valor;
+
+    factory ScanModel.fromJson(Map<String, dynamic> json) => ScanModel(
+        id: json["id"],
+        valor: json["valor"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "valor": valor,
+    };
+}
